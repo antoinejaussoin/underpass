4fcf2f25148b30276b631d155a85a55d723c4ea4
Antoine Jaussoin <antoine.jaussoin@rbs.com>
2014-07-01 15:19:33 +0100

    Adding zip stream

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ var express = require('express');
 var http = require('http');
 var request = require('request');
 var router = express.Router();
+var packer = require('zip-stream');
 
 /* GET home page. */
 router.get('/', function(req, res) {
@@ -15,10 +16,30 @@ router.post('/', function(req, res, next) {
 	console.log(url);
 	console.log(fileName);
 
-	//res.setHeader("content-type", "application/octet-stream");
+	var archive = new packer();
+
+	archive.on('error', function(err) {
+	  throw err;
+	});
+
 	var file = request(url);
-	res.setHeader('Content-disposition', 'attachment; filename='+fileName);
-	file.pipe(res);
+	res.setHeader('Content-disposition', 'attachment; filename='+fileName+'.zip');
+	archive.pipe(res);
+
+	// pipe archive where you want it (ie fs, http, etc)
+	// listen to the destination's end, close, or finish event
+
+	archive.entry(file, { name: fileName }, function(err, entry) {
+	  if (err) throw err;
+	   archive.finalize();
+	});
+
+
+
+	//res.setHeader("content-type", "application/octet-stream");
+	
+	
+	//file.pipe(res);
 
 
 	

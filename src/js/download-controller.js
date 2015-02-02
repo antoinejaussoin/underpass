823c585e4753a7d299cb9997e66198054622cb0c
Antoine Jaussoin <antoine.jaussoin@rbs.com>
2015-02-02 16:03:46 +0000

    Fixing form but not working yet

--- src/js/download-controller.js
+++ src/js/download-controller.js
@@ -1,49 +1,52 @@
 (function () {
     'use strict';
 
-	var app = angular.module('underpass');
+    var app = angular.module('underpass');
 
-	app.controller('downloadController', function($scope, $http) {
-		$scope.link = '';
-		$scope.name = '';
-		$scope.isValid = false;
-		$scope.isDownloading = false;
-		$scope.downloadCount = 0;
+    app.controller('downloadController', function ($scope, $http) {
+        $scope.link = '';
+        $scope.name = '';
+        $scope.isValid = false;
+        $scope.isDownloading = false;
+        $scope.downloadList = [];
 
-		$scope.download = function() {
-			$scope.downloadCount ++;
-			$scope.isDownloading = true;
-		}
+        $scope.download = function () {
+            $scope.isDownloading = true;
+            $scope.downloadList.push({
+                link: $scope.link,
+                name: $scope.name
+            });
+        }
 
-		$scope.downloadAgain = function(){
-			$scope.isDownloading = false;
-			$scope.link = '';
-			$scope.name = '';
-		}
+        $scope.downloadAgain = function () {
+            $scope.isDownloading = false;
+            $scope.link = '';
+            $scope.name = '';
+        }
 
-		function guessFileName(){
-			var splits  = $scope.link.split('/');
-			var possibleFileName = splits[splits.length - 1];
-			var fileNameSplits = possibleFileName.split('.');
-			if (fileNameSplits.length > 1){
-				var extension = fileNameSplits[fileNameSplits.length - 1];
+        function guessFileName() {
+            var splits = $scope.link.split('/');
+            var possibleFileName = splits[splits.length - 1];
+            var fileNameSplits = possibleFileName.split('.');
+            if (fileNameSplits.length > 1) {
+                var extension = fileNameSplits[fileNameSplits.length - 1];
 
-				if (extension.length === 3){
-					$scope.name = possibleFileName;
-					return;
-				}
-			}
+                if (extension.length === 3) {
+                    $scope.name = possibleFileName;
+                    return;
+                }
+            }
 
-			$scope.name = '';
-		}
+            $scope.name = '';
+        }
 
-		function setValidity(){
-			$scope.isValid = $scope.link.length > 8 && $scope.name.length >= 3;
-		}
+        function setValidity() {
+            $scope.isValid = $scope.link.length > 8 && $scope.name.length >= 3;
+        }
 
-		$scope.$watch('link', guessFileName);
-		$scope.$watch('link', setValidity);
-		$scope.$watch('name', setValidity);
-	});
+        $scope.$watch('link', guessFileName);
+        $scope.$watch('link', setValidity);
+        $scope.$watch('name', setValidity);
+    });
 
 })();
